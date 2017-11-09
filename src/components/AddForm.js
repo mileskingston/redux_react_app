002ff5d9782dff569705c951e71ea4cd1f06c724
Miles Kingston <miles.kingston@dixonscarphone.com>
2017-11-09 11:06:35 +0000

    added components folder and form validation

---
+++ src/components/AddForm.js
@@ -0,0 +1,60 @@
+  import React from 'react';
+  import { connect } from 'react-redux';
+  import { addToDos } from '../actions/toDos';
+
+  import './AddForm.css';
+
+  let input;
+
+  class AddForm extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        isValid: true
+      };
+
+      this.addTodo = this.addTodo.bind(this);
+    }
+
+    addTodo(e) {
+      e.preventDefault();
+      
+      if (input.value) {
+        this.props.addToDos(input.value);
+        input.value = '';
+      } else {
+        this.setState({ isValid: false });        
+      }
+    }
+    
+    render() {
+      const {state, props} = this;
+
+      return (
+        <form id="addForm" className="add-form" onSubmit={this.addTodo} noValidate>
+          <input 
+            ref={node => {input = node}} 
+            className="add-form__input input" 
+            type="text"
+            minLength="3"
+            onFocus={() => this.setState({ isValid: true })}
+            required
+          />
+          <button className="add-form__btn btn" type="submit">Add</button>
+          {
+            !state.isValid &&
+            <p className="add-form__error error">This field is required</p>
+          }
+        </form>
+      );
+    }
+  }
+
+  function mapStateToProps(state) {
+    return {
+      todos: state
+    }
+  }
+
+  export default connect(mapStateToProps, {addToDos})(AddForm);
